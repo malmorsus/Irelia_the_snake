4cfc545fd1c23b124b96c61b83a4c03dbef490f0
fed-putilov <fed-putilov@yandex.ru>
2021-04-24 23:56:33 +0300

    add AI scripts

---
+++ Assets/ShootingEnemyAI.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemyAI : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    public float patrollingMoveSpeed;
+    private float waitTime;
+    public float startWaitTimeBtwPatrolling;
+    public float moveSpeedTowardsPlayer;
+    public float agroRange;
+    public float retreatDistance;
+    public Transform player;
+
+    [Header("Move Spots")]
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    [Header("Shooting Stuff")]
+    public GameObject projectile;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwShots = startTimeBtwShots;
+        waitTime = startWaitTimeBtwPatrolling;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < agroRange)
+        {
+            AttackPlayer();
+        }
+        else
+        {
+            Patroll();
+        }
+
+
+
+    }
+
+    void AttackPlayer()
+    {
+        if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -moveSpeedTowardsPlayer * Time.deltaTime);
+        }
+        else
+        {
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        }
+    }
+
+
+    void Patroll()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, patrollingMoveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTimeBtwPatrolling;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/WeakEnemyAI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakEnemyAI : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    public float patrollingMoveSpeed;
+    private float waitTime;
+    public float startWaitTimeBtwPatrolling;
+    public float moveSpeedTowardsPlayer;
+    public float agroRange;
+    public Transform player;
+
+    [Header("Move Spots")]
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        waitTime = startWaitTimeBtwPatrolling;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patroll();
+        }
+
+
+        
+    }
+
+    void ChasePlayer()
+    {
+        if (Vector2.Distance(transform.position, player.position) > 0.2f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeedTowardsPlayer * Time.deltaTime);
+        }
+    }
+
+
+    void Patroll()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, patrollingMoveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTimeBtwPatrolling;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+
+}
