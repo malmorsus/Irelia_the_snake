634f3747ad00f24714eb13678e7a5019087acc55
fed-putilov <fed-putilov@yandex.ru>
2021-04-27 01:38:05 +0300

    level 1

--- Assets/Scripts/Activator.cs
+++ Assets/Scripts/Activator.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Activator : MonoBehaviour
 {
     public List<EnemyHealth> AliveEnemys = new List<EnemyHealth>();
 
+    private int scene;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene().buildIndex;
+    }
+
     void Update()
     {
-        if (AliveEnemys.Count == 0)
+        if (AliveEnemys.Count <= 0)
         {
-            //Òóò
+            SceneManager.LoadScene(scene + 1);
         }
 
     }
